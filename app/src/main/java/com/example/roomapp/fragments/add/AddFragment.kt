43d3ec7f6070869d93fea6d919ef701e5d787552
Marcel Fuchs <68725691+Juggertek@users.noreply.github.com>
2020-09-26 10:04:06 +0200

    Try/Catch Block funktioniert.

--- app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
@@ -1,8 +1,10 @@
 package com.example.roomapp.fragments.add
 
 import android.os.Bundle
+import android.os.Message
 import android.text.Editable
 import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -43,23 +45,29 @@ class AddFragment : Fragment() {
         val age = addAge_et.text
         val dob = addDob_et.text
 
-        if (inputCheck(firstName, lastName, age, dob)) {
-            // Create User Object
-            val user = User(
-                0,
-                firstName,
-                lastName,
-                Integer.parseInt(age.toString()),
-                Date.valueOf(dob.toString())
-            )
-            // Add Data to Database
-            mUserViewModel.addUser(user)
-            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_LONG).show()
-            // Navigate Back
-            findNavController().navigate(R.id.action_addFragment_to_listFragment)
-        } else {
-            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG)
-                .show()
+        try {
+            if (inputCheck(firstName, lastName, age, dob)) {
+                // Create User Object
+                val user = User(
+                    0,
+                    firstName,
+                    lastName,
+                    Integer.parseInt(age.toString()),
+                    Date.valueOf(dob.toString())
+                )
+                // Add Data to Database
+                mUserViewModel.addUser(user)
+                Toast.makeText(
+                    requireContext(), "Successfully added!", Toast.LENGTH_LONG
+                ).show()
+                // Navigate Back
+                findNavController().navigate(R.id.action_addFragment_to_listFragment)
+            } else {
+                Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }catch (e:Throwable){
+            Toast.makeText(requireContext(),"Please enter the dob in the correct format: YYYY-MM-DD",Toast.LENGTH_LONG).show()
         }
     }
 
